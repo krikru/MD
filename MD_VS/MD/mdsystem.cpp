280194eb65c8610c4929e25286c91f4a2807e27f
krikr808 <krikr808@student.liu.se>
2011-11-15 19:11:49 +0100

    Normal distributed (almost) initial particle velocities

--- MD_VS/MD/mdsystem.cpp
+++ MD_VS/MD/mdsystem.cpp
@@ -424,7 +424,10 @@ void mdsystem::init_particles() {
     float sum_sqr_vel = 0;
     for (uint i = 0; i < nrparticles; i++) {
         for (uint j = 0; j < 3; j++) {
-            particles[i].start_vel[j] = ((float) rand())/((float) RAND_MAX) - 0.5f;
+            particles[i].start_vel[j] = 0;
+            for (uint terms = 0; terms < 10; terms++) { //This will effectivelly create a distribution very similar to normal distribution.
+                particles[i].start_vel[j] += float(rand());
+            }
         }
         sum_vel    += particles[i].start_vel;
         sum_sqr_vel += particles[i].start_vel.sqr_length();
