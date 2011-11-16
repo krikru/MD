d37ccfd1e13c4c6658f02b8f63017e853836ef22
emika143 <emika143@student.liu.se>
2011-11-16 16:00:26 +0100

    Emil

--- MD_VS/MD/main.cpp
+++ MD_VS/MD/main.cpp
@@ -33,7 +33,7 @@ int _tmain(int argc, _TCHAR* argv[])
     uint nrinst_in = 10;          // Number of timesteps between measurements of properties
     uint nrtimesteps_in = 500; // Desired (or minimum) total number of timesteps
     float inner_cutoff_in = 2.0f * sigma_in;
-    float outer_cutoff_in = 1.01f * inner_cutoff_in; //Fewer neighbors -> faster, but too thin skin is not good either.
+    float outer_cutoff_in = 1.1f * inner_cutoff_in; //Fewer neighbors -> faster, but too thin skin is not good either.
 
     // Init flags
     bool diff_c_on_in = true;
